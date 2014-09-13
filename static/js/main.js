d009d9cd96012611c1d9c7074924a24f5486b783
Yuji Tomita <yuchant@gmail.com>
2014-09-13 17:03:38 -0400

    Changed intro completely

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,19 @@
 $(function() {
-	var degrees = 180;
-	$(".flamingo").click(function() {
-		$(".flamingo-animation-wrapper").toggleClass('is-flamingo');
-	})
+	// var degrees = 180;
+	// $(".flamingo").click(function() {
+	// 	$(".flamingo-animation-wrapper").toggleClass('is-flamingo');
+	// })
 
-	$(".flamingo").on('mouseenter', function() {
-		var number = Math.random() * ($(window).width()/2 * .9);
-		if (Math.random() < .5) {
-			// 50% chance of negative version
-			number = number * -1;
-		}
-		$(this).css({
-			'-moz-transform': 'translateX(' + number + 'px)',
-			'-webkit-transform': 'translateX(' + number + 'px)',
-			'transform': 'translateX(' + number + 'px)'
-		})
-	})
+	// $(".flamingo").on('mouseenter', function() {
+	// 	var number = Math.random() * ($(window).width()/2 * .9);
+	// 	if (Math.random() < .5) {
+	// 		// 50% chance of negative version
+	// 		number = number * -1;
+	// 	}
+	// 	$(this).css({
+	// 		'-moz-transform': 'translateX(' + number + 'px)',
+	// 		'-webkit-transform': 'translateX(' + number + 'px)',
+	// 		'transform': 'translateX(' + number + 'px)'
+	// 	})
+	// })
 })
