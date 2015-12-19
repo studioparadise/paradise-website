62ef8678d41b79f1b128a927a9d01e8ec6e27424
Yuji Tomita <yuchant@gmail.com>
2015-12-18 19:40:00 -0500

    Initial

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,56 @@
-$(function() {
-	// var degrees = 180;
-	// $(".flamingo").click(function() {
-	// 	$(".flamingo-animation-wrapper").toggleClass('is-flamingo');
-	// })
+components = {};
 
-	// $(".flamingo").on('mouseenter', function() {
-	// 	var number = Math.random() * ($(window).width()/2 * .9);
-	// 	if (Math.random() < .5) {
-	// 		// 50% chance of negative version
-	// 		number = number * -1;
-	// 	}
-	// 	$(this).css({
-	// 		'-moz-transform': 'translateX(' + number + 'px)',
-	// 		'-webkit-transform': 'translateX(' + number + 'px)',
-	// 		'transform': 'translateX(' + number + 'px)'
-	// 	})
-	// })
+components.stickyElement = function($element, args) {
+  var controller;
+  controller = {};
+  controller.isSticky = false;
+  controller.init = function() {
+    var $window;
+    $window = $(window);
+    $window.on('scroll', function() {
+      var offset, scrollTop;
+      scrollTop = $window.scrollTop();
+      offset = $element.offset().top;
+      if (args.offset) {
+        offset -= Number(args.offset);
+      }
+      if (offset < scrollTop) {
+        controller.enableSticky();
+      } else {
+        controller.disableSticky();
+      }
+    });
+  };
+  controller.enableSticky = function() {
+    $element.addClass('is-sticky');
+    controller.isSticky = true;
+  };
+  controller.disableSticky = function() {
+    $element.removeClass('is-sticky');
+    controller.isSticky = false;
+  };
+  controller.init();
+  return args;
+};
+
+
+$(function() {
+
+	$(".swiper-container").swiper({
+		scrollbar: '.swiper-scrollbar',
+		scrollbarHide: false,
+		scrollbarDraggable: true,
+		scrollbarSnapOnRelease: true
+	})
+
+	var $intro = $(".paradise-intro");
+
+	components.stickyElement($intro, {
+
+	})
+
+	var $hello = $(".paradise-hello");
+	$(window).on('scroll', function() {
+
+	});
 })
