527f8a13f63dd6fd24a18ba0e3ef8a8693ab2c88
Yuji Tomita <yuchant@gmail.com>
2016-05-28 00:19:52 -0400

    Add debug output and inviewport

--- _site/static/coffee/init.js
+++ _site/static/coffee/init.js
@@ -1,5 +1,5 @@
 (function() {
-  var root;
+  var debug, root;
 
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
@@ -19,10 +19,33 @@
     return root.controllers[name]($("[js-controller=\"" + name + "\"]"));
   };
 
-  root.controllers.navbar = function($element, args) {};
+  root.controllers.navbar = function($element, args) {
+    var calculateOffsetForProject, getCurrentProject;
+    console.log('navbar');
+    calculateOffsetForProject = function(project) {};
+    getCurrentProject = function() {
+      var $projects, i, len, output, project, tolerance;
+      tolerance = $(window).height() * .3;
+      $projects = $("[js-index-project]:in-viewport(" + tolerance + ")");
+      output = '';
+      for (i = 0, len = $projects.length; i < len; i++) {
+        project = $projects[i];
+        output += ' ' + $(project).attr('js-index-project');
+      }
+      return debug(output);
+    };
+    return $(window).on('scroll', function() {
+      return getCurrentProject();
+    });
+  };
+
+  debug = function(msg) {
+    return $("[js-debug-txt]").html(msg);
+  };
 
   $(function() {
-    return root.utils.bindOneController('indexProject');
+    root.utils.bindOneController('indexProject');
+    return root.utils.bindOneController('navbar');
   });
 
 }).call(this);
